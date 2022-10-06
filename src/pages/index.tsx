ab2bea0ae5b56924e3bacfe3bc5db88e7851d46d
kawamou <kwmkzsd@gmail.com>
2022-10-06 23:40:30 +0900

    wip: Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import type { NextPage } from 'next'
 
+const Header = () => {
+  return (
+    <div className='navbar h-20'>
+      <div className='flex h-full'>
+        <div className='flex-1 m-auto text-xl'>aaa</div>
+        <div className='mt-auto mb-auto text-xl'>bbb</div>
+        <div className='mt-auto mb-auto ml-8 text-xl'>ccc</div>
+        <div className='mt-auto mb-auto ml-8 text-xl'>ddd</div>
+      </div>
+    </div>
+  )
+}
 const Home: NextPage = () => {
   return <>top page</>
 }
