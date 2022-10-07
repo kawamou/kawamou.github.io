5536147cd4d08aba42201b1b7495fd658b3ce3a1
kawamou <kwmkzsd@gmail.com>
2022-10-07 09:26:40 +0900

    useStateで管理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useState, useCallback } from 'react'
 
 const Header = () => {
   return (
@@ -36,62 +37,75 @@ const AboutMe = () => {
     </div>
   )
 }
+
 type Experience = {
-  id: number
   title: string
   date: string
   description: string
 }
 
-const experiences: Experience[] = [
-  {
-    id: 0,
-    title: '',
-    date: '',
-    description: '',
-  },
-]
-
 // https://portfolio-tailwind.preview.uideck.com/demos/personal/
 // My Resumeの部分を埋める
 // https://flowbite.com/docs/components/timeline/
 // これが良さそう
-const Experiences = (timelines: Experience[]) => {
+const Experiences = () => {
+  const [experiences] = useState<Experience[]>([
+    {
+      title: 'ここにタイトルが入る',
+      date: '2022.01.01',
+      description:
+        'ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。',
+    },
+    {
+      title: 'ここにタイトルが入る',
+      date: '2022.01.01',
+      description:
+        'ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。',
+    },
+    {
+      title: 'ここにタイトルが入る',
+      date: '2022.01.01',
+      description:
+        'ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。',
+    },
+  ])
   return (
-    <div>
+    <div className='max-w-3xl ml-auto mr-auto'>
       <ol className='relative border-l border-gray-200'>
-        {timelines.map((timeline) => {
+        {experiences?.map((experience, index) => {
           return (
             <>
-              <li className='mb-10 ml-6'>
-                <span className='flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900'>
-                  <svg
-                    aria-hidden='true'
-                    className='w-3 h-3 text-blue-600 dark:text-blue-400'
-                    fill='currentColor'
-                    viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fill-rule='evenodd'
-                      d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-                      clip-rule='evenodd'
-                    ></path>
-                  </svg>
-                </span>
+              <span className='flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900'>
+                <svg
+                  aria-hidden='true'
+                  className='w-3 h-3 text-blue-600 dark:text-blue-400'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    fill-rule='evenodd'
+                    d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
+                    clip-rule='evenodd'
+                  ></path>
+                </svg>
+              </span>
+              {index ? (
+                <></>
+              ) : (
                 <h3 className='flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white'>
-                  Flowbite Application UI v2.0.0{' '}
+                  {experience.title}
                   <span className='bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3'>
                     Latest
                   </span>
                 </h3>
-                <time className='block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
-                  {timeline.date}
-                </time>
-                <p className='mb-4 text-base font-normal text-gray-500 dark:text-gray-400'>
-                  {timeline.description}
-                </p>
-              </li>
+              )}
+              <time className='block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
+                {experience.date}
+              </time>
+              <p className='mb-4 text-base font-normal text-gray-500 dark:text-gray-400'>
+                {experience.description}
+              </p>
             </>
           )
         })}
