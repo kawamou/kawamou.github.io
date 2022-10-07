8c32873a3193a8bea5593ae54cbd3e9c3a1c798d
kawamou <kwmkzsd@gmail.com>
2022-10-08 01:21:10 +0900

    heroiconsの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import { useState, useCallback } from 'react'
+import { BriefcaseIcon } from '@heroicons/react/outline'
 
 const Header = () => {
   return (
@@ -76,30 +77,18 @@ const Experiences = () => {
           return (
             <>
               <span className='flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900'>
-                <svg
-                  aria-hidden='true'
-                  className='w-3 h-3 text-blue-600 dark:text-blue-400'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    fill-rule='evenodd'
-                    d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-                    clip-rule='evenodd'
-                  ></path>
-                </svg>
+                <BriefcaseIcon></BriefcaseIcon>
               </span>
-              {index ? (
-                <></>
-              ) : (
-                <h3 className='flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white'>
-                  {experience.title}
+              <h3 className='flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white'>
+                {experience.title}
+                {index > 0 ? (
+                  <></>
+                ) : (
                   <span className='bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3'>
                     Latest
                   </span>
-                </h3>
-              )}
+                )}
+              </h3>
               <time className='block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
                 {experience.date}
               </time>
