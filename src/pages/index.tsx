04f0026844bed59a64c4648a091a19e9fe895d20
kawamou <kwmkzsd@gmail.com>
2022-10-14 00:03:47 +0900

    Header修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,12 +20,9 @@ const SideNav = () => {
 
 const Header = () => {
   return (
-    <div className='navbar h-20'>
-      <div className='flex h-full'>
-        <div className='flex-1 m-auto text-xl'>aaa</div>
-        <div className='mt-auto mb-auto text-xl'>bbb</div>
-        <div className='mt-auto mb-auto ml-8 text-xl'>ccc</div>
-        <div className='mt-auto mb-auto ml-8 text-xl'>ddd</div>
+    <div className='navbar h-20 p-4'>
+      <div className='flex h-full max-w-3xl ml-auto mr-auto'>
+        <div className='flex-1 m-auto text-xl'>kawamou</div>
       </div>
     </div>
   )
