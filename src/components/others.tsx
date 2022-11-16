4ddcb09999e100d0dee469fa8a4da2e1bd34efc5
kawamou <kwmkzsd@gmail.com>
2022-11-17 00:09:48 +0900

    活性可能に変更

--- src/components/others.tsx
+++ src/components/others.tsx
@@ -1,6 +1,8 @@
 import { Card, Typography } from '@material-tailwind/react'
-import { title } from 'process'
 import { FC, useState } from 'react'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 type OtherProps = {
   title: string
@@ -9,17 +11,48 @@ type OtherProps = {
 }
 
 const Other: FC<OtherProps> = ({ title, subTitle, hideDescription }) => {
+  const [showDetail, setShowDetail] = useState(false)
   return (
-    <div className='my-2'>
-      <Typography className='text-sm mb-1 text-blue-gray-900'>{title}</Typography>
-      <Typography color='gray' className='text-sm mb-2 '>
-        {subTitle}
-      </Typography>
+    <div className='px-6 py-3 hover:bg-gray-50'>
+      <button
+        className='w-full'
+        onClick={() => {
+          setShowDetail(!showDetail)
+        }}
+      >
+        <div className='flex items-center'>
+          <div>
+            <Typography className='text-sm mb-1 text-blue-gray-900'>{title}</Typography>
+            <Typography color='gray' className='text-sm text-left'>
+              {subTitle}
+            </Typography>
+          </div>
+          {showDetail ? (
+            <ChevronUpIcon className='w-4 ml-auto duration-1000'></ChevronUpIcon>
+          ) : (
+            <ChevronDownIcon className='w-4 ml-auto duration-1000'></ChevronDownIcon>
+          )}
+        </div>
+      </button>
+      <div
+        className={`${
+          showDetail && hideDescription.length > 0 ? 'mt-4 h-6 opacity-100' : 'h-0 opacity-0'
+        } duration-300 overflow-hidden`}
+      >
+        <a target='_blank' rel='noreferrer' href={`${hideDescription}`} className='inline-bloc'>
+          <div className='flex items-center'>
+            <Typography className='text-sm text-blue-gray-900 text-left mr-2 hover:border-b border-blue-gray-400'>
+              {hideDescription}
+            </Typography>
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={'h-2'} />
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
 
-export const Others = () => {
+const Others = () => {
   const [others] = useState<OtherProps[]>([
     {
       title: 'ドコモ夏季インターン2022 ハッカソンメンター',
@@ -73,11 +106,19 @@ export const Others = () => {
     },
   ])
   return (
-    <Card className='p-6 shadow-sm mb-6'>
-      <Typography className='text-xl font-bold mb-4 text-blue-gray-900'>Others</Typography>
+    <Card className='shadow-sm mb-6'>
+      <Typography className='px-6 pt-6 text-xl font-bold mb-4 text-blue-gray-900'>
+        Others
+      </Typography>
       {others.map((other, i) => {
-        return <Other key={i} {...other}></Other>
+        return (
+          <div key={i} className={`border-t border-gray-200`}>
+            <Other key={i} {...other}></Other>
+          </div>
+        )
       })}
     </Card>
   )
 }
+
+export default Others
