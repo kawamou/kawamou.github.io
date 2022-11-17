e31ba570dabc5d01a6d56d0c55a7a284cfead03a
kawamou <kwmkzsd@gmail.com>
2022-11-18 00:11:32 +0900

    レスポンシブ対応

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,48 +5,46 @@ import { useState } from 'react'
 export const Header = () => {
   const [showDetail, setShowDetail] = useState(false)
   return (
-    <Navbar className='min-w-full py-2 px-4 lg:px-8 lg:py-4 rounded-none border-none shadow-sm mb-16'>
+    <Navbar className='min-w-full px-4 py-4 rounded-none border-none shadow-sm mb-16'>
       <div className='container mx-auto flex items-center justify-between text-blue-gray-900'>
         <Typography className='mr-4 cursor-pointer font-bold text-blue-gray-900'>
           <span>Hi👋</span>
         </Typography>
-        <div className='flex gap-2 items-center'>
-          <Avatar src='/yellow.png' variant='circular' size='xs'></Avatar>
-          <div>
-            <button
-              className='flex gap-1 items-center'
-              onClick={() => {
-                setShowDetail(!showDetail)
-              }}
-            >
-              <Typography className='text-sm font-medium'>kawamou</Typography>
-              {showDetail ? (
-                <ChevronUpIcon className='w-3'></ChevronUpIcon>
-              ) : (
-                <ChevronDownIcon className='w-3'></ChevronDownIcon>
-              )}
-            </button>
+        <div>
+          <button
+            className='flex gap-1 items-center'
+            onClick={() => {
+              setShowDetail(!showDetail)
+            }}
+          >
+            <Avatar src='/yellow.png' variant='circular' size='xs'></Avatar>
+            <Typography className='ml-1 text-sm font-medium'>kawamou</Typography>
             {showDetail ? (
-              <div className='mt-2 absolute z-50 list-none rounded-lg border bg-white text-left text-sm'>
-                <ul className=''>
-                  <li className='py-1 px-4 hover:bg-gray-100'>
-                    <div
-                      onClick={() => {
-                        return
-                      }}
-                    >
-                      Edit
-                    </div>
-                  </li>
-                  <li className='py-1 px-4 hover:bg-gray-100'>
-                    <div>Delete</div>
-                  </li>
-                </ul>
-              </div>
+              <ChevronUpIcon className='w-3'></ChevronUpIcon>
             ) : (
-              <></>
+              <ChevronDownIcon className='w-3'></ChevronDownIcon>
             )}
-          </div>
+          </button>
+          {showDetail ? (
+            <div className='mt-2 absolute list-none rounded-lg border bg-white text-left text-sm'>
+              <ul className=''>
+                <li className='py-4 px-6 hover:bg-gray-100'>
+                  <div
+                    onClick={() => {
+                      return
+                    }}
+                  >
+                    Edit
+                  </div>
+                </li>
+                <li className='py-4 px-6 hover:bg-gray-100'>
+                  <div>Delete</div>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </Navbar>
