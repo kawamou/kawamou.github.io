ba1412f6f6ff1df56a612f1885ff137f84e9e985
kawamou <kwmkzsd@gmail.com>
2022-11-17 14:53:49 +0900

    Menuを追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
 import { Avatar, Navbar, Typography } from '@material-tailwind/react'
-import { ChevronDownIcon } from '@heroicons/react/outline'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline'
+import { useState } from 'react'
 
 export const Header = () => {
+  const [showDetail, setShowDetail] = useState(false)
   return (
     <Navbar className='min-w-full py-2 px-4 lg:px-8 lg:py-4 rounded-none border-none shadow-sm mb-16'>
       <div className='container mx-auto flex items-center justify-between text-blue-gray-900'>
@@ -10,9 +12,40 @@ export const Header = () => {
         </Typography>
         <div className='flex gap-2 items-center'>
           <Avatar src='/yellow.png' variant='circular' size='xs'></Avatar>
-          <div className='flex gap-1'>
-            <Typography className='text-sm font-medium'>kawamou</Typography>
-            <ChevronDownIcon className='w-3'></ChevronDownIcon>
+          <div>
+            <button
+              className='flex gap-1 items-center'
+              onClick={() => {
+                setShowDetail(!showDetail)
+              }}
+            >
+              <Typography className='text-sm font-medium'>kawamou</Typography>
+              {showDetail ? (
+                <ChevronUpIcon className='w-3'></ChevronUpIcon>
+              ) : (
+                <ChevronDownIcon className='w-3'></ChevronDownIcon>
+              )}
+            </button>
+            {showDetail ? (
+              <div className='mt-2 absolute z-50 list-none rounded-lg border bg-white text-left text-sm'>
+                <ul className=''>
+                  <li className='py-1 px-4 hover:bg-gray-100'>
+                    <div
+                      onClick={() => {
+                        return
+                      }}
+                    >
+                      Edit
+                    </div>
+                  </li>
+                  <li className='py-1 px-4 hover:bg-gray-100'>
+                    <div>Delete</div>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
