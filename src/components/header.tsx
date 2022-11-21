ee5ee9424947159a02b586092f2a2b605773ec29
kawamou <kwmkzsd@gmail.com>
2022-11-22 01:08:00 +0900

    relativeにすることでz-index対応

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,12 @@ import { useState } from 'react'
 export const Header = () => {
   const [showDetail, setShowDetail] = useState(false)
   return (
-    <Navbar className='min-w-full px-4 py-4 rounded-none border-none shadow-sm mb-16'>
+    <Navbar className='min-w-full px-4 py-4 rounded-none border-none shadow-sm mb-16 relative z-10'>
       <div className='container mx-auto flex items-center justify-between text-blue-gray-900'>
         <Typography className='mr-4 cursor-pointer font-bold text-blue-gray-900'>
           <span>Hi👋</span>
         </Typography>
-        <div>
+        <div className='static'>
           <button
             className='flex gap-1 items-center'
             onClick={() => {
@@ -25,26 +25,33 @@ export const Header = () => {
               <ChevronDownIcon className='w-3'></ChevronDownIcon>
             )}
           </button>
-          {showDetail ? (
-            <div className='mt-2 absolute list-none rounded-lg border bg-white text-left text-sm'>
-              <ul className=''>
-                <li className='py-4 px-6 hover:bg-gray-100'>
-                  <div
-                    onClick={() => {
-                      return
-                    }}
-                  >
-                    Edit
-                  </div>
-                </li>
-                <li className='py-4 px-6 hover:bg-gray-100'>
-                  <div>Delete</div>
-                </li>
-              </ul>
-            </div>
-          ) : (
-            <></>
-          )}
+          <div
+            className={`${
+              showDetail ? '' : 'hidden'
+            } mt-2 absolute z-50 list-none rounded-lg border bg-white text-left text-sm`}
+          >
+            <ul className=''>
+              <li className='py-2 px-8 hover:bg-gray-100'>
+                <div
+                  onClick={() => {
+                    return
+                  }}
+                >
+                  <Typography className='text-sm text-blue-gray-900'>工事中👷‍♂️</Typography>
+                </div>
+              </li>
+              <li className='py-2 px-8 hover:bg-gray-100'>
+                <div>
+                  <Typography className='text-sm text-blue-gray-900'>工事中👷‍♂️</Typography>
+                </div>
+              </li>
+              <li className='py-2 px-8 hover:bg-gray-100'>
+                <div>
+                  <Typography className='text-sm text-blue-gray-900'>工事中👷‍♂️</Typography>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </Navbar>
