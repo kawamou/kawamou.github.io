59835bc3a7414db577e2de2d20b5ee4ac6c28b9d
kawamou <kwmkzsd@gmail.com>
2022-11-22 01:09:05 +0900

    Skillsの修正

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -30,7 +30,7 @@ const Skills = () => {
   ])
   return (
     <Card className='shadow-sm p-6'>
-      <Typography className='text-xl font-bold text-blue-gray-900'>Preffered Positons</Typography>
+      {/* <Typography className='text-xl font-bold text-blue-gray-900'>Preffered Position</Typography>
       <ul className='list-disc p-4 border-b marker:text-blue-gray-900 border-gray-200'>
         <li className='pb-3'>
           <div className='max-w-sm rounded-xl border border-gray-200'>
@@ -40,11 +40,11 @@ const Skills = () => {
               </div>
               <FontAwesomeIcon
                 icon={faCheckCircle}
-                className='text-green-500 pr-3'
+                className='text-green-500 pr-3 w-4'
               ></FontAwesomeIcon>
               <div className='border-l border-gray-200'>
                 <div className='px-3 py-2'>
-                  <Typography className='text-sm text-blue-gray-900'>2 Year</Typography>
+                  <Typography className='text-sm text-blue-gray-900'>2.5 Year</Typography>
                 </div>
               </div>
             </div>
@@ -60,21 +60,22 @@ const Skills = () => {
               </div>
               <FontAwesomeIcon
                 icon={faCheckCircle}
-                className='text-green-500 pr-3'
+                className='text-green-500 pr-3 w-4'
               ></FontAwesomeIcon>
               <div className='border-l border-gray-200'>
                 <div className='px-3 py-2'>
-                  <Typography className='text-sm text-blue-gray-900'>2 Year</Typography>
+                  <Typography className='text-sm text-blue-gray-900'>2.5 Year</Typography>
                 </div>
               </div>
             </div>
           </div>
         </li>
-      </ul>
-      <Typography className='text-xl font-black my-4 text-blue-gray-900'>Skills</Typography>
+      </ul> */}
+      <Typography className='text-xl font-black text-blue-gray-900 mb-4'>Skills</Typography>
       <div>
         <Typography className='text-sm mb-6 text-blue-gray-900'>
-          業務ではうんぬんかんぬん。プライベートではうんぬんかんぬん
+          本番環境で使ったことがあるスキルとしてはGoやGoogle
+          Cloud、Firebase、AWS、コンテナが挙げられます。最近はプライベートでTypeScriptやReactを触っています
         </Typography>
       </div>
       <div className='flex gap-2 flex-wrap'>
