925660f9509d677d57952ec7d9f21a36690ba021
kawamou <kwmkzsd@gmail.com>
2023-06-19 12:21:05 +0900

    Skillsを修正

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -33,52 +33,12 @@ const Skills = () => {
   ])
   return (
     <Card className='shadow-sm p-6'>
-      {/* <Typography className='text-xl font-bold text-blue-gray-900'>Preffered Position</Typography>
-      <ul className='list-disc p-4 border-b marker:text-blue-gray-900 border-gray-200'>
-        <li className='pb-3'>
-          <div className='max-w-sm rounded-xl border border-gray-200'>
-            <div className='flex items-center gap-2'>
-              <div className='mr-auto pl-3'>
-                <Typography className='text-sm text-blue-gray-900'>バックエンド（Go）</Typography>
-              </div>
-              <FontAwesomeIcon
-                icon={faCheckCircle}
-                className='text-green-500 pr-3 w-4'
-              ></FontAwesomeIcon>
-              <div className='border-l border-gray-200'>
-                <div className='px-3 py-2'>
-                  <Typography className='text-sm text-blue-gray-900'>2.5 Year</Typography>
-                </div>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className='pb-3'>
-          <div className='max-w-sm rounded-xl border border-gray-200'>
-            <div className='flex items-center gap-2'>
-              <div className='mr-auto pl-3'>
-                <Typography className='text-sm text-blue-gray-900'>
-                  クラウド / SRE（Google Cloud）
-                </Typography>
-              </div>
-              <FontAwesomeIcon
-                icon={faCheckCircle}
-                className='text-green-500 pr-3 w-4'
-              ></FontAwesomeIcon>
-              <div className='border-l border-gray-200'>
-                <div className='px-3 py-2'>
-                  <Typography className='text-sm text-blue-gray-900'>2.5 Year</Typography>
-                </div>
-              </div>
-            </div>
-          </div>
-        </li>
-      </ul> */}
       <Typography className='text-xl font-black text-blue-gray-900 mb-4'>Skills</Typography>
       <div>
         <Typography className='text-sm mb-6 text-blue-gray-900 leading-6'>
-          Go や Google Cloud、Firebase
-          を中心にバックエンドの設計と開発ができます。また新規事業経験を活かし、いわゆるスタートアップフィットジャーニーの各過程への貢献ができます。
+          主に Go や TypeScript、Next.js、Google Cloud / Firebase を使って Web
+          プロダクトを作るのが得意です。新規事業の立ち上げからクローズまで経験アリ。最近は R&D
+          Engineer として自己研鑽中。
         </Typography>
       </div>
       <div className='flex gap-2 flex-wrap'>
